580caf8b9ef2a156a6f35ed226aea639dcc2a934
shivam <gargshivam482@gmail.com>
2019-09-24 15:10:28 +0530

    test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Avtaar-Generator!');
+    expect(page.getParagraphText()).toEqual('Welcome to f-a-g-a!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Avtaar-Generator'`, async(() => {
+  it(`should have as title 'f-a-g-a'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Avtaar-Generator');
+    expect(app.title).toEqual('f-a-g-a');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Avtaar-Generator!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to f-a-g-a!');
   }));
 });
